b56383769c2d8245a281b47a182755b240ed7c6a
Vera Martins <124790502+Verona03@users.noreply.github.com>
2024-10-23 04:38:30 -0300

    tentar mandar para a riot e ganhar uma Skin
    brincadeira nao finalizada

---
+++ jinx.py
@@ -0,0 +1,12 @@
+import pymsgbox
+vitima = pymsgbox.prompt('MÃOZUDA?! Onde você esta?')
+
+
+pymsgbox.alert('Botão de segurança.', 'SOS', 'SOS_SOS')
+
+
+pymsgbox.alert('JINX WAS HERE!', 'Piltover ALERT!!!!!', "PILTS SERÃO ESCAMADOS, FISHBONES!")
+pymsgbox .confirm('Destruir o conselho?', 'Confirmar destruição',[ "Sim, tenho certeza." ,  'Cancelar' ], timeout = 2000)
+tkinter.messagebox.ERROR = ('JINX WAS HERE!', 'Piltover ALERT!!!!!', "PILTS SERÃO ESCAMADOS, FISHBONES!")
+#pymsgbox .confirm ( 'Destruir o site da órbita?' ,  'Confirmar destruição' ,  [ "Sim, tenho certeza." ,  'Cancelar' ],  timeout = 2000 )   # fecha após 2000 milissegundos (2 segundos)
+#"Timeout"
